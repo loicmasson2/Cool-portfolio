695561e4982fa1d5e578f5f1636cee3457c52d69
Loïc Masson <masson.loic.023@gmail.com>
2020-11-27 19:43:18 +0200

    feature/implement background for the second section, still not as the wireframe but the mechanism is here

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,15 +2,21 @@
 import React from 'react';
 import { Box } from 'rebass';
 import BackgroundPrimaryLight from '../components/Backgrounds/BackgroundPrimaryLight.png';
+import BackgroundSecondaryLight from '../components/Backgrounds/BackgroundSecondaryLight.png';
 
-const Background = ({ children }) => (
+const backgroundMapping = {
+  landing: BackgroundPrimaryLight,
+  about: BackgroundSecondaryLight,
+};
+
+const Background = ({ name, children }) => (
   <Box
     sx={{
       px: 6,
       py: 6,
       backgroundRepeat: 'no-repeat',
-      backgroundImage: `url(${BackgroundPrimaryLight})`,
-      backgroundSize: '100%',
+      backgroundImage: `url(${backgroundMapping[name]})`,
+      backgroundSize: '100% 50%',
       backgroundPosition: '0 bottom',
       borderRadius: 8,
     }}
